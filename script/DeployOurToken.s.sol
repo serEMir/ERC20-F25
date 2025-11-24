b8e2e561734c9a20c2bf19b87a4c1c3ef33ce070
seremir <monksenzu@gmail.com>
2025-11-24 15:30:52 +0100

    Initial commit

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -7,7 +7,7 @@ import {OurToken} from "../src/OurToken.sol";
 contract DeployOurToken is Script {
     uint256 constant INITIAL_SUPPLY = 100 ether;
 
-    function run() external returns(OurToken) {
+    function run() external returns (OurToken) {
         vm.startBroadcast();
         OurToken ot = new OurToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
